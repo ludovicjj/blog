727fc38358126d8a989b1c51d5fbb29c7be2ff52
Votre nom ou pseudo <jahanlud@gmail.com>
2018-08-13 20:50:45 +0200

    into posts, add loop foreach to create list of posts

--- master/view/frontend/posts.php
+++ master/view/frontend/posts.php
@@ -1,4 +1,10 @@
-<h1>voici la liste des posts</h1>
+<div class="col-sm-8">
+	<?php foreach($post AS $post): ?>
+	<h1><?= $post->getTitle();?></h1>
+	<small>Par <?= $post->getAuthor();?>, le <?= $post->getDay() .' '.$post->getMonth().' '.$post->getYear();?></small>
+	<p><?= $post->getIntro();?></p>
+	<?php endforeach; ?>
+</div>
 
 <div class="col-sm-8">
 	<?php for($i = 0; $i < $paging; $i++): ?>
