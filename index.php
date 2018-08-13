20966c49d82129fa5269a17be171c573567f5fc9
Votre nom ou pseudo <jahanlud@gmail.com>
2018-08-13 20:39:53 +0200

    require for all class

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
+require ('master/model/MasterFactory.php');
+require ('master/model/database/MysqlDatabase.php');
+
+require ('master/controller/Controller.php');
+require ('master/controller/PostsController.php');
+
+require ('master/model/table/Table.php');
+require ('master/model/table/PostsTable.php');
+
+require ('master/model/entity/Entity.php');
+require ('master/model/entity/PostsEntity.php');
+
 if(isset($_GET['p']))
 {
 	$p = $_GET['p'];
