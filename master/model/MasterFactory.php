f21b14defe8e25608d667626c40d52463acc1387
Votre nom ou pseudo <jahanlud@gmail.com>
2018-08-13 19:44:39 +0200

    add into class MasterFactory design pattern singleton

--- master/model/MasterFactory.php
+++ master/model/MasterFactory.php
@@ -5,4 +5,15 @@ namespace master\model;
 class MasterFactory
 {
 	
+	private static $_instance;
+	
+	public static function getInstance()
+	{
+		if(self::$_instance === null)
+		{
+			self::$_instance = new MasterFactory();
+		}
+		return self::$_instance;
+	}
+	
 }
