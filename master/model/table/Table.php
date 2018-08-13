017b580ee6bf38cdd9b9696e1de550164c320d52
Votre nom ou pseudo <jahanlud@gmail.com>
2018-08-13 19:59:25 +0200

    into class Table, define two variables by __construct

--- master/model/table/Table.php
+++ master/model/table/Table.php
@@ -4,5 +4,21 @@ namespace master\model\table;
 
 class Table
 {
+	protected $db;
+	protected $table;
 	
+	public function __construct( \master\model\database\MysqlDatabase $db)
+	{
+		$this->db = $db;
+		
+		if($this->table === null)
+		{
+			$class_name = (get_class($this));
+			$morceaux = explode('\\', $class_name);
+			$lastmorceau = end($morceaux);
+			$resultat = strtolower(str_replace('Table', '', $lastmorceau));
+			$this->table = $resultat;
+		}
+		
+	}
 }
