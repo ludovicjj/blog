70d1dd20258eae62ecd588091a5053eadef9fae1
Votre nom ou pseudo <jahanlud@gmail.com>
2018-08-13 20:48:55 +0200

    into posts, add loop for create paging

--- master/view/frontend/posts.php
+++ master/view/frontend/posts.php
@@ -1,1 +1,7 @@
 <h1>voici la liste des posts</h1>
+
+<div class="col-sm-8">
+	<?php for($i = 0; $i < $paging; $i++): ?>
+	<a class="btn btn-primary" href="index.php?p=posts&page=<?=$i+1;?>"><?= $i+1; ?></a>
+	<?php endfor; ?>
+</div>
