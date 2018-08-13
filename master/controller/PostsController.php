f38d76691205d20e729056933004b63e00bb3151
Votre nom ou pseudo <jahanlud@gmail.com>
2018-08-13 20:31:31 +0200

    into class PostsController, create function posts

--- master/controller/PostsController.php
+++ master/controller/PostsController.php
@@ -4,5 +4,32 @@ namespace master\controller;
 
 class PostsController extends Controller
 {
-	
+	public function posts()
+	{
+		$master = MasterFactory::getInstance();
+		$req = $master->getTable('posts')->paging();
+		
+		$paging =  ceil($req['page'] / 4);
+		$paging = (int) $paging;
+		
+		if(isset($_GET['page']))
+		{
+			$limit = ($_GET['page'] * 4) - 4;
+		}
+		else
+		{
+			$limit = 0;
+		}
+		
+		$req_post = $master->getTable('posts')->all($limit);
+		$post = array();
+		foreach($req_post AS $data)
+		{
+			$entityPost = $master->getTable('posts')->getEntity($data);
+			$post[] = $entityPost;
+		}
+		
+		
+		$this->render('frontend/posts', compact('paging', 'post'));
+	}
 }
