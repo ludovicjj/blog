9f1ec782472e3447abe8d0ad82d23956b8c5d5ba
Votre nom ou pseudo <jahanlud@gmail.com>
2018-08-13 20:11:13 +0200

    into PostsTable, create function all

--- master/model/table/PostsTable.php
+++ master/model/table/PostsTable.php
@@ -19,4 +19,21 @@ class PostsTable extends Table
 		true);
 		return $req;
 	}
+	
+	/** function all
+	* @param int
+	* @return array
+	*/
+	public function all($limit)
+	{
+		$req = $this->db->prepareWithLimit(
+		'SELECT posts.id, posts.title, posts.intro, posts.content, posts.author, posts.image,
+		MONTH(date_post) AS month, DAY(date_post) AS day, YEAR(date_post) AS year, DATE_FORMAT(date_post, \'%H:%i:%s\') AS time
+		FROM '. $this->table .'
+		ORDER BY date_post DESC
+		LIMIT :limit, 4',
+		$limit
+		);
+		return $req;
+	}
 }
