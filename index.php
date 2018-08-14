6e3fcdee8a1175101d7b2832d4748293d5758cbe
Votre nom ou pseudo <jahanlud@gmail.com>
2018-08-14 05:56:27 +0200

    into index, delete require to all class and replace them by function load

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
 <?php
 
 require ('master/model/MasterFactory.php');
-require ('master/model/database/MysqlDatabase.php');
-
-require ('master/controller/Controller.php');
-require ('master/controller/PostsController.php');
-
-require ('master/model/table/Table.php');
-require ('master/model/table/PostsTable.php');
-
-require ('master/model/entity/Entity.php');
-require ('master/model/entity/PostsEntity.php');
+master\model\MasterFactory::load();
 
 if(isset($_GET['p']))
 {
