2ab82d95460d36800dd25f105ee62e99dc54716e
Votre nom ou pseudo <jahanlud@gmail.com>
2018-08-14 05:50:58 +0200

    into Autoloader, create function register and autoload

--- master/Autoloader.php
+++ master/Autoloader.php
@@ -4,5 +4,15 @@ namespace master;
 
 class Autoloader
 {
-
+	static function register()
+	{
+		spl_autoload_register(array(__CLASS__, 'autoload'));
+	}
+	
+	static function autoload($class)
+	{
+		$class = str_replace( __NAMESPACE__ . '\\', '', $class);
+		$class = str_replace('\\', '/', $class);
+		require __DIR__ .'/' . $class . '.php';
+	}
 }
