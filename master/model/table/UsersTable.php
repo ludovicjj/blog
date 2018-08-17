3c56d3a1cbed0b5269edbdd782578506045c7f46
Votre nom ou pseudo <jahanlud@gmail.com>
2018-08-17 12:55:59 +0200

    into UsersTable, create function usersExists

--- master/model/table/UsersTable.php
+++ master/model/table/UsersTable.php
@@ -4,5 +4,21 @@ namespace master\model\table;
 
 class UsersTable extends Table
 {
-	
+	/** function usersExists
+	* @param string username
+	* @param string mail
+	* @return array
+	*/
+	public function usersExists($username, $mail)
+	{
+		$req = $this->db->prepare(
+		'SELECT COUNT(*) AS user
+		FROM '. $this->table .'
+		WHERE username = ? OR mail = ?',
+		[$username, $mail],
+		true
+		);
+		
+		return $req;
+	}
 }
