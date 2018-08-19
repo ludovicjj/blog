e0a568841fa95809bb7daa2c8099724384d41c81
Votre nom ou pseudo <jahanlud@gmail.com>
2018-08-19 15:46:09 +0200

    into UsersTable, fix code to follow PSR rules

--- master/model/table/UsersTable.php
+++ master/model/table/UsersTable.php
@@ -1,32 +1,31 @@
 <?php
-
 namespace master\model\table;
 
 class UsersTable extends Table
 {
-	/** function usersExists
-	* @param string username
-	* @param string mail
-	* @return array
-	*/
-	public function usersExists($username, $mail)
-	{
-		$req = $this->db->prepare(
-		'SELECT COUNT(*) AS user
-		FROM '. $this->table .'
-		WHERE username = ? OR mail = ?',
-		[$username, $mail],
-		true
-		);
-		
-		return $req;
-	}
+    /*
+    * function usersExists
+    * @param string username
+    * @param string mail
+    * @return array
+    */
+    public function usersExists($username, $mail)
+    {
+        $req = $this->db->prepare(
+            'SELECT COUNT(*) AS user
+            FROM '. $this->table .'
+            WHERE username = ? OR mail = ?',
+            [$username, $mail],
+            true
+        );
+        return $req;
+    }
 	
-	public function addUser($username, $password, $mail)
-	{
-		$req = $this->db->prepare(
-		'INSERT INTO '. $this->table .' SET username = ?, password = ?, mail = ?',
-		[$username, $password, $mail]
-		);
-	}
+    public function addUser($username, $password, $mail)
+    {
+        $req = $this->db->prepare(
+            'INSERT INTO '. $this->table .' SET username = ?, password = ?, mail = ?',
+            [$username, $password, $mail]
+        );
+    }
 }
