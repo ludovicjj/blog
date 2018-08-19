77b728027cca45806b1a6e34ef123f342a45b25e
Votre nom ou pseudo <jahanlud@gmail.com>
2018-08-19 15:42:38 +0200

    Into PostsTable, fix code to follow PSR rules

--- master/model/table/PostsTable.php
+++ master/model/table/PostsTable.php
@@ -1,50 +1,58 @@
 <?php
-
 namespace master\model\table;
 
 class PostsTable extends Table
 {
-	/** function paging
-	* @return array
-	*/
-	public function paging()
-	{
-		$req = $this->db->query(
-		'SELECT COUNT(*) AS page 
-		FROM '. $this->table .'',
-		true);
-		return $req;
-	}
+    /*
+    * function paging
+    * @return array
+    */
+    public function paging()
+    {
+        $req = $this->db->query(
+            'SELECT COUNT(*) AS page FROM '. $this->table .'',
+            true
+        );
+        return $req;
+    }
 	
-	/** function all
-	* @param int
-	* @return array
-	*/
-	public function all($limit)
-	{
+    /*
+	* function all
+    * @param int
+    * @return array
+    */
+    public function all($limit)
+    {
 		$req = $this->db->prepareWithLimit(
-		'SELECT posts.id, posts.title, posts.intro, posts.content, posts.author, posts.image,
-		MONTH(date_post) AS month, DAY(date_post) AS day, YEAR(date_post) AS year, DATE_FORMAT(date_post, \'%H:%i:%s\') AS hour
-		FROM '. $this->table .'
-		ORDER BY date_post DESC
-		LIMIT :limit, 4',
-		$limit
+            'SELECT posts.id, posts.title, posts.intro, posts.content, posts.author, posts.image,
+            MONTH(date_post) AS month,
+            DAY(date_post) AS day,
+            YEAR(date_post) AS year,
+            DATE_FORMAT(date_post, \'%H:%i:%s\') AS hour
+            FROM '. $this->table .'
+            ORDER BY date_post DESC
+            LIMIT :limit, 4',
+            $limit
 		);
 		return $req;
-	}
+    }
 	
-	/** function postWithId
-	* @return array
-	*/
-	public function postWithId($id)
-	{
+    /*
+    * function postWithId
+    * @return array
+    */
+    public function postWithId($id)
+    {
 		$req = $this->db->prepare(
-		'SELECT posts.id, posts.title, posts.intro, posts.content, posts.author, posts.image,
-		MONTH(date_post) AS month, DAY(date_post) AS day, YEAR(date_post) AS year, DATE_FORMAT(date_post, \'%H:%i:%s\') AS hour
-		FROM '. $this->table .'
-		WHERE id = ?',
-		[$id], true
+            'SELECT posts.id, posts.title, posts.intro, posts.content, posts.author, posts.image,
+            MONTH(date_post) AS month,
+            DAY(date_post) AS day,
+            YEAR(date_post) AS year,
+            DATE_FORMAT(date_post, \'%H:%i:%s\') AS hour
+            FROM '. $this->table .'
+            WHERE id = ?',
+            [$id], true
 		);
 		return $req;
-	}
+    }
 }
