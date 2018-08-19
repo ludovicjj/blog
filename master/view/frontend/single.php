0202438b3b0ef7b33f9679e8973684f548738e47
Votre nom ou pseudo <jahanlud@gmail.com>
2018-08-19 15:55:28 +0200

    into signle, replace tabs by space for indenting

--- master/view/frontend/single.php
+++ master/view/frontend/single.php
@@ -1,6 +1,6 @@
 <div class="col-sm-12">
-	<h1><?= $post->getTitle();?></h1>
-	<small>Par <?= $post->getAuthor();?>, le <?= $post->getDay() .' '.$post->getMonth().' '.$post->getYear();?></small>
-	<p><strong><?= $post->getIntro();?></strong></p>
-	<p><?= $post->getContent();?></p>
+    <h1><?= $post->getTitle();?></h1>
+    <small>Par <?= $post->getAuthor();?>, le <?= $post->getDay() .' '.$post->getMonth().' '.$post->getYear();?></small>
+    <p><strong><?= $post->getIntro();?></strong></p>
+    <p><?= $post->getContent();?></p>
 </div>
