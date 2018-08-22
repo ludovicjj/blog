672dd51b0d20a62d276c90ac4db70cc745dc774b
Votre nom ou pseudo <jahanlud@gmail.com>
2018-08-22 16:30:51 +0200

    Into AdminController, create construct and add construct parent

--- master/controller/admin/AdminController.php
+++ master/controller/admin/AdminController.php
@@ -3,5 +3,8 @@ namespace master\controller\admin;
 
 class AdminController extends \master\controller\Controller
 {
-	
+    public function __construct()
+    {
+        parent::__construct();
+    }
 }
