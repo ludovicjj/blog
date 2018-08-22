06e55bcc3bdafac3ac58173c460217506323066d
Votre nom ou pseudo <jahanlud@gmail.com>
2018-08-22 11:40:37 +0200

    into UsersController, update function register

--- master/controller/UsersController.php
+++ master/controller/UsersController.php
@@ -22,9 +22,13 @@ class UsersController extends Controller
                 if ($entity->getMail() === null) {
                     $message = 'Le format de l\'adresse email est incorrect';
                 } else {
-                    $req = $users->usersExists($entity->getUsername(), $entity->getMail());
+                    $req = $users->usersExists($_POST['username'], $_POST['mail']);
                     if ($req['user'] == 0) {
-                        $users->addUser($entity->getUsername(), $entity->getPassword(), $entity->getMail());
+                        $users->addUser(
+                            $_POST['username'],
+                            sha1($_POST['password']),
+                            $_POST['mail']
+                        );
                         $error = false;
                         $message = 'Inscription réussie';
                     } else {
