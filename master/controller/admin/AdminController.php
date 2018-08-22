9a31241f354ffc50264f22a2e48c1d57830ed0e9
Votre nom ou pseudo <jahanlud@gmail.com>
2018-08-22 17:01:45 +0200

    Into AdminController, add function auth and refused and update its construct

--- master/controller/admin/AdminController.php
+++ master/controller/admin/AdminController.php
@@ -6,5 +6,24 @@ class AdminController extends \master\controller\Controller
     public function __construct()
     {
         parent::__construct();
+
+        if(!$this->auth()) {
+            $this->refused();
+        }
+    }
+
+    private function auth()
+    {
+        if (isset($_SESSION['statut']) && $_SESSION['statut'] == 2) {
+            return $_SESSION['statut'];
+        } else {
+            return false;
+        }
+    }
+	
+    protected function refused()
+    {
+        $this->render('frontend/error/refused');
+        die();
     }
 }
