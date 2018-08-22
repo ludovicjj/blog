4f59a9c417133bcc933454493e3acd26ba1810be
Votre nom ou pseudo <jahanlud@gmail.com>
2018-08-22 19:20:42 +0200

    into UsersController (admin), create function upper and function down

--- master/controller/admin/UsersController.php
+++ master/controller/admin/UsersController.php
@@ -16,4 +16,18 @@ class UsersController extends AdminController
         }
         $this->render('backend/users/index', compact('users'));
     }
+	
+    public function upper()
+    {
+        $master = MasterFactory::getInstance();
+        $req_users = $master->getTable('users')->upUser($_GET['id']);
+        header('Location:index.php?p=admin.users.index');
+    }
+	
+    public function down()
+    {
+        $master = MasterFactory::getInstance();
+        $req_users = $master->getTable('users')->downUser($_GET['id']);
+        header('Location:index.php?p=admin.users.index');
+    }
 }
