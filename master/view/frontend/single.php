21a8c0f3734b866c313b41570284726dbe549a7d
Votre nom ou pseudo <jahanlud@gmail.com>
2018-08-22 12:07:18 +0200

    into single, create form and message

--- master/view/frontend/single.php
+++ master/view/frontend/single.php
@@ -4,3 +4,22 @@
     <p><strong><?= $post->getIntro();?></strong></p>
     <p><?= $post->getContent();?></p>
 </div>
+
+<div class="col-sm-12">
+    <legend>Commentaires</legend>
+    <?php 
+    if (isset($_SESSION['username'])) {
+    ?>
+    <form method="post" class="form-group">
+        <div class="form-group">
+            <label for="content">Votre message</label>
+            <textarea id="content" name="content" class="form-control"></textarea>
+        </div>
+        <button class="btn btn-primary">Envoyer</button>
+    </form>
+    <?php
+    } else {
+        echo '<div class="alert alert-danger">Vous devez être connecté pour envoyer un commentaire</div>';
+    }
+    ?>
+</div>
