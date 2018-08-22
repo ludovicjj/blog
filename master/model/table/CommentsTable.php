b684a233a8427d49d7d31aae9590332c6d38503b
Votre nom ou pseudo <jahanlud@gmail.com>
2018-08-22 12:28:57 +0200

    into CommentsTable, create function sendComment

--- master/model/table/CommentsTable.php
+++ master/model/table/CommentsTable.php
@@ -3,5 +3,17 @@ namespace master\model\table;
 
 class CommentsTable extends Table
 {
-	
+    /*
+    * function sendComment
+    * @param string content
+    * @param int post_id
+    * @param string author 
+    */
+    public function sendComment($content, $post_id, $author)
+    {
+        $req = $this->db->prepare(
+            'INSERT INTO '. $this->table .' SET content = ?, post_id = ?, author = ?',
+            [$content, $post_id, $author]
+        );
+    }
 }
