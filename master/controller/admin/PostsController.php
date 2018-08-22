ab159357bf7f14122dec73355d47d357d194b119
Votre nom ou pseudo <jahanlud@gmail.com>
2018-08-22 17:12:32 +0200

    Into PostsController (admin), create function add

--- master/controller/admin/PostsController.php
+++ master/controller/admin/PostsController.php
@@ -15,5 +15,29 @@ class PostsController extends AdminController
             $posts[] = $entity_posts;
         }
         $this->render('backend/posts/index', compact('posts'));
-    }	
+    }
+
+    public function add()
+    {
+        $master = MasterFactory::getInstance();
+		$error = null;
+		$message = null;
+
+		if ($_POST) {
+			$error = true;
+			$message = 'Tous les champs sont obligatoires';
+			
+			if (!empty($_POST['title'] && $_POST['intro'] && $_POST['content'] && $_POST['author'] && $_POST['image'])) {
+				$req_posts = $master->getTable('posts')->addPost(
+					$_POST['title'],
+					$_POST['intro'],
+					$_POST['content'],
+					$_POST['author'],
+					$_POST['image']
+				);
+				header('Location:index.php?p=admin.posts.index');
+			}
+		}
+        $this->render('backend/posts/add', compact('error', 'message'));
+    }
 }
