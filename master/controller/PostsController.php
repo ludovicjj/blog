107efa7766ddf4c4dbb4d2a8543a0de5a8bd7801
Votre nom ou pseudo <jahanlud@gmail.com>
2018-08-31 13:48:04 +0200

    Replace tabs to space

--- master/controller/PostsController.php
+++ master/controller/PostsController.php
@@ -9,31 +9,31 @@ class PostsController extends Controller
     {
         $master = MasterFactory::getInstance();
         $req = $master->getTable('posts')->paging();
-		
+        
         $paging =  ceil($req['page'] / 4);
         $paging = (int) $paging;
-		
+        
         if (isset($_GET['page'])) {
             $limit = ($_GET['page'] * 4) - 4;
 		} else {
             $limit = 0;
         }
-		
+        
         $req_post = $master->getTable('posts')->allWithLimit($limit);
         $post = array();
         foreach ($req_post as $data) {
             $entity_post = $master->getTable('posts')->getEntity($data);
             $post[] = $entity_post;
         }
-		
+        
         $this->render('frontend/posts', compact('paging', 'post'));
     }
-	
+    
     public function index()
     {
         $this->render('frontend/index');
     }
-	
+    
     public function singlePost()
     {
         if (isset($_GET['id'])) {
