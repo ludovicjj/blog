0ddc8df9724d97f28fa5d2914fa361f378b0fc67
Votre nom ou pseudo <jahanlud@gmail.com>
2018-08-31 22:00:56 +0200

    delete tabs and whitespace after openning parenthesis

--- master/Autoloader.php
+++ master/Autoloader.php
@@ -7,10 +7,10 @@ class Autoloader
     {
         spl_autoload_register(array(__CLASS__, 'autoload'));
     }
-	
+    
     private static function autoload($class)
     {
-        $class = str_replace( __NAMESPACE__ . '\\', '', $class);
+        $class = str_replace(__NAMESPACE__ . '\\', '', $class);
         $class = str_replace('\\', '/', $class);
         require __DIR__ .'/' . $class . '.php';
     }
