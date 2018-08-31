f052b3685080d23f2bfec3b1ca13893fbea976d2
Votre nom ou pseudo <jahanlud@gmail.com>
2018-08-31 20:04:47 +0200

    fix codacy errors

--- master/model/table/Table.php
+++ master/model/table/Table.php
@@ -6,27 +6,26 @@ class Table
     protected $db;
     protected $table;
     protected $entity;
-	
+    
     public function __construct( \master\model\database\MysqlDatabase $db)
     {
         $this->db = $db;
-		
+        
         if ($this->table === null) {
             $class_name = (get_class($this));
             $morceaux = explode('\\', $class_name);
             $last_morceau = end($morceaux);
             $resultat = strtolower(str_replace('Table', '', $last_morceau));
             $this->table = $resultat;
-        }	
+        }
     }
 	
     public function getEntity($array)
     {
         $class_name = get_class($this);
-        $explode = explode('\\' , $class_name);
+        $explode = explode('\\', $class_name);
         $last = end($explode);
         $resultat = 'master\model\entity\\'.str_replace('Table', 'Entity', $last);
-		
         return $this->entity = new $resultat($array);
     }
 }
