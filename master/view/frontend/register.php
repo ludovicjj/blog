887decd09edbec2efefab713688c38e50af0f641
Votre nom ou pseudo <jahanlud@gmail.com>
2018-09-01 17:53:28 +0200

    build html for register page

--- master/view/frontend/register.php
+++ master/view/frontend/register.php
@@ -1,26 +1,36 @@
-<div class="col-sm-8">
-    <form class="form-group" method="post">
-        <legend>formulaire d'inscription</legend>
-        <label for="username">Pseudo : </label>
-        <input id="username" type="text" class="form-control" name="username">
-		
-        <label for="password">Mot de passe : </label>
-        <input id="password" type="password" class="form-control" name="password">
-		
-        <label for="mail">Mail : </label>
-        <input id="mail" type="text" class="form-control" name="mail">
-		
-        <br>
-        <button class="btn btn-primary">Envoyer</button>
-    </form>
-	
-<?php
-if ($error !== null) {
-    if ($error) {
-        echo '<div class="alert alert-danger">'. $message .'</div>';
-    } else {
-        echo '<div class="alert alert-success">'. $message .'</div>';
-    }
-}
-?>
+<div class="container-fluid conteneur">
+    <div class="container">
+        <div class="col-sm-8 bloc-register">
+            <form class="form-group" method="post">
+                <legend>Inscription</legend>
+                
+                <div class="form-group conteneur-form">
+                    <label for="username">Pseudo : </label>
+                    <input id="username" type="text" class="form-control champs" name="username" placeholder="Pseudo">
+                </div>
+                
+                <div class="form-group conteneur-form">
+                    <label for="password">Mot de passe : </label>
+                    <input id="password" type="password" class="form-control champs" name="password" placeholder="Mot de passe">
+                </div>
+                
+                <div class="form-group conteneur-form">
+                    <label for="mail">Mail : </label>
+                    <input id="mail" type="text" class="form-control champs" name="mail" placeholder="Email">
+                </div>
+                
+                <button class="btn btn-custom">Envoyer</button>
+            </form>
+            
+        <?php
+        if ($error !== null) {
+            if ($error) {
+                echo '<div class="alert alert-danger">'. $message .'</div>';
+            } else {
+                echo '<div class="alert alert-success">'. $message .'</div>';
+            }
+        }
+        ?>
+        </div>
+    </div>
 </div>
