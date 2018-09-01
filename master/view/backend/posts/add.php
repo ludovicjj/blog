e24259d6cee05e75db9911ca5fd28af32da1e9ce
Votre nom ou pseudo <jahanlud@gmail.com>
2018-09-01 18:11:28 +0200

    update html add post page (admin)

--- master/view/backend/posts/add.php
+++ master/view/backend/posts/add.php
@@ -1,30 +1,37 @@
-<form method="post" class="form-group">
-	<legend>Ajouter un article</legend>
-	<div class="form-group">
-		<label for="title">Titre</label>
-		<input id="title" name="title" class="form-control"/>
-	</div>
-	<div class="form-group">
-		<label for="intro">Introduction</label>
-		<textarea id="intro" name="intro" class="form-control"></textarea>
-	</div>
-	<div class="form-group">
-		<label for="content">Contenu</label>
-		<textarea id="content" name="content" class="form-control"></textarea>
-	</div>
-	<div class="form-group">
-		<label for="author">Auteur</label>
-		<input id="author" name="author" class="form-control" value="<?= $_SESSION['username'];?>"/>
-	</div>
-	<div class="form-group">
-		<label for="image">Image</label>
-		<input id="image" name="image" class="form-control"/>
-	</div>
-	<button type="submit"class="btn btn-primary">Envoyer</button>
-</form>
+<div class="container-fuild conteneur">
+    <div class="container admin">        
+        <a href="index.php?p=admin.posts.index" class="back-admin-index">Retour à la gestion des articles</a>
+        
+        <form method="post" class="form-group">
+            <legend>Ajouter un article</legend>
+            <div class="form-group">
+                <label for="title">Titre</label>
+                <input id="title" name="title" class="form-control champs"/>
+            </div>
+            <div class="form-group">
+                <label for="intro">Introduction</label>
+                <textarea id="intro" name="intro" class="form-control champs"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="content">Contenu</label>
+                <textarea id="content" name="content" class="form-control champs"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="author">Auteur</label>
+                <input id="author" name="author" class="form-control champs" value="<?= $_SESSION['username'];?>"/>
+            </div>
+            <div class="form-group">
+                <label for="image">Image</label>
+                <input id="image" name="image" class="form-control champs"/>
+            </div>
+            
+            <button type="submit"class="btn btn-custom">Envoyer</button>
+        </form>
 
-<?php
-if (isset($error) && $error === true) {
-    echo '<div class="alert alert-danger">'.$message.'</div>';
-}
-?>
+        <?php
+        if (isset($error) && $error === true) {
+            echo '<div class="alert alert-danger">'.$message.'</div>';
+        }
+        ?>
+    </div>
+</div>
