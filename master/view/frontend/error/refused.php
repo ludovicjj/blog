011b861bc7a5c00f9f43deb2f8912cfcdce88932
Votre nom ou pseudo <jahanlud@gmail.com>
2018-09-01 19:06:54 +0200

    update html refused pade with container fluid and container

--- master/view/frontend/error/refused.php
+++ master/view/frontend/error/refused.php
@@ -1,2 +1,6 @@
-<h1>Acces refusé</h1>
-<a href="index.php?p=index">Retour à l'accueil</a>
+<div class="container-fluid conteneur">
+    <div class="container">
+        <h1>Acces refusé</h1>
+        <a href="index.php?p=index">Retour à l'accueil</a>
+    </div>
+</div>
