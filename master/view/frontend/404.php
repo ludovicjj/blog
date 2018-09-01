2426462388dee76b8254f6c794b7825efc17acbf
Votre nom ou pseudo <jahanlud@gmail.com>
2018-09-01 18:03:06 +0200

    update html for error page

--- master/view/frontend/404.php
+++ master/view/frontend/404.php
@@ -1,2 +1,6 @@
-<h1>Page introuvable</h1>
-<p><a href="index.php">Retour a la page d'accueil</a></p>
+<div class="container-fluid conteneur">
+    <div class="container">        
+        <h1>Page introuvable</h1>
+        <p><a href="index.php">Retour a la page d'accueil</a></p>
+    </div>
+</div>
