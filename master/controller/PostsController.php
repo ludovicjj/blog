5ad40a511937a95e7738262f22280afab879e642
Votre nom ou pseudo <jahanlud@gmail.com>
2018-09-03 03:26:14 +0200

    add space after comma, rename variable with short name

--- master/controller/PostsController.php
+++ master/controller/PostsController.php
@@ -45,11 +45,11 @@ class PostsController extends Controller
                 $message = 'Format de l\'adresse Email incorrect.';
                 if (preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['mail'])) {
                     $from = $_POST['mail'];
-                    $to = "jahanblog@alwaysdata.net";
+                    $target = "jahanblog@alwaysdata.net";
                     $subject = "Contact via blog";
                     $message = htmlspecialchars($_POST['message']);
                     $headers = "From:" . $from;
-                    mail($to,$subject,$message, $headers);
+                    mail($target, $subject, $message, $headers);
                     header('Location:index.php?p=index&info=send');
                 }
             }
