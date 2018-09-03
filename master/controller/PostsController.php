c9578dac3077180a72cde4ee8a3063d3a1f3d657
Votre nom ou pseudo <jahanlud@gmail.com>
2018-09-03 03:01:23 +0200

    check form before using function mail

--- master/controller/PostsController.php
+++ master/controller/PostsController.php
@@ -36,6 +36,24 @@ class PostsController extends Controller
     public function index()
     {
         $this->setTitle('Accueil');
+        $error = null;
+        $message = null;
+        if ($_POST) {
+            $error = true;
+            $message = 'Tous les champs sont obligatoires.';
+            if (!empty($_POST['nom'] && $_POST['mail'] && $_POST['message'])) {
+                $message = 'Format de l\'adresse Email incorrect.';
+                if (preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['mail'])) {
+                    $from = $_POST['mail'];
+                    $to = "jahanblog@alwaysdata.net";
+                    $subject = "Contact via blog";
+                    $message = htmlspecialchars($_POST['message']);
+                    $headers = "From:" . $from;
+                    mail($to,$subject,$message, $headers);
+                    header('Location:index.php?p=index&info=send');
+                }
+            }
+        }
         $this->render('frontend/index');
     }
     
