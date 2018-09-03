581bdf260955cea21423cf6d9f01034123168580
Votre nom ou pseudo <jahanlud@gmail.com>
2018-09-03 04:51:19 +0200

    change class for input and textarea from admin page

--- master/view/backend/posts/edit.php
+++ master/view/backend/posts/edit.php
@@ -6,23 +6,28 @@
             <legend>Modifier un article</legend>
             <div class="form-group">
                 <label for="title">Titre</label>
-                <input id="title" name="title" class="form-control champs" value="<?=$post->getTitle();?>"/>
+                <input id="title" name="title" class="form-control champs-admin" 
+                value="<?=$post->getTitle();?>"/>
             </div>
             <div class="form-group">
                 <label for="intro">Introduction</label>
-                <textarea id="intro" name="intro" class="form-control champs"><?=$post->getIntro();?></textarea>
+                <textarea id="intro" name="intro" 
+                class="form-control champs-admin"><?=$post->getIntro();?></textarea>
             </div>
             <div class="form-group">
                 <label for="content">Contenu</label>
-                <textarea id="content" name="content" class="form-control champs"><?=$post->getContent();?></textarea>
+                <textarea id="content" name="content" 
+                class="form-control champs-admin"><?=$post->getContent();?></textarea>
             </div>
             <div class="form-group">
                 <label for="author">Auteur</label>
-                <input id="author" name="author" class="form-control champs" value="<?= $_SESSION['username'];?>"/>
+                <input id="author" name="author" 
+                class="form-control champs-admin" value="<?= $_SESSION['username'];?>"/>
             </div>
             <div class="form-group">
                 <label for="image">Image</label>
-                <input id="image" name="image" class="form-control champs" value="<?=$post->getImage();?>"/>
+                <input id="image" name="image" 
+                class="form-control champs-admin" value="<?=$post->getImage();?>"/>
             </div>
             
             <button type="submit"class="btn btn-custom">Envoyer</button>
