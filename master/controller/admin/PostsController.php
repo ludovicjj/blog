0db6bb2a09ac2ba15e0293ba301d1a6943a46730
Votre nom ou pseudo <jahanlud@gmail.com>
2018-09-04 16:25:17 +0200

    Update method add and edit with check image field

--- master/controller/admin/PostsController.php
+++ master/controller/admin/PostsController.php
@@ -31,15 +31,33 @@ class PostsController extends AdminController
                 $_POST['intro'] &&
                 $_POST['content'] &&
                 $_POST['author'] &&
-                $_POST['image'])) {
-                $master->getTable('posts')->addPost(
-                    $_POST['title'],
-                    $_POST['intro'],
-                    $_POST['content'],
-                    $_POST['author'],
-                    $_POST['image']
-                );
-                header('Location:index.php?p=admin.posts.index');
+                $_FILES['image']['name'])) {
+                    
+                $image = $_FILES['image']['name'];
+                $extensions_valides = array('jpg', 'jpeg', 'png');
+                $extension_upload = strtolower(substr(strrchr($image, '.'), 1));
+                
+                if ($_FILES['image']['error'] > 0) {
+                    $error = "Erreur lors du transfert de l\'image";
+                } else {
+                    if (in_array($extension_upload, $extensions_valides)) {
+                        $path_image ='public/img/' . $_FILES['image']['name'];
+                        move_uploaded_file(
+                            $_FILES['image']['tmp_name'],
+                            $path_image
+                        );
+                        $master->getTable('posts')->addPost(
+                            $_POST['title'],
+                            $_POST['intro'],
+                            $_POST['content'],
+                            $_POST['author'],
+                            $path_image
+                        );
+                        header('Location:index.php?p=admin.posts.index');
+                    } else {
+                        $error = 'Extension incorrecte (uniquement ".jpg", ".jpeg", ".png")';
+                    }
+                }
             }
         }
         $this->render('backend/posts/add', compact('error', 'message'));
@@ -47,23 +65,40 @@ class PostsController extends AdminController
     
     public function edit()
     {
+        $path_image = null;
+        
         if (isset($_GET['id'])) {
             $master = MasterFactory::getInstance();
             $req_post = $master->getTable('posts')->postWithId($_GET['id']);
             if ($req_post) {
                 $post = $master->getTable('posts')->getEntity($req_post);
                 if ($_POST) {
+                    if (empty($_FILES['image']['name'])) {
+                        $path_image = $post->getImage();
+                    } else {
+                        $image = $_FILES['image']['name'];
+                        $extensions_valides = array('jpg', 'jpeg', 'png');
+                        $extension_upload = strtolower(substr(strrchr($image, '.'), 1));
+                        
+                        if (in_array($extension_upload, $extensions_valides)) {
+                            $path_image ='public/img/' . $_FILES['image']['name'];
+                            move_uploaded_file(
+                                $_FILES['image']['tmp_name'],
+                                $path_image
+                            ); 
+                        }  
+                    }
                     $master->getTable('posts')->addPostWithId(
                         $_POST['title'],
                         $_POST['intro'],
                         $_POST['content'],
                         $_POST['author'],
-                        $_POST['image'],
+                        $path_image,
                         $_GET['id']
                     );
                     header('Location:index.php?p=admin.posts.index');
                 }
-                $this->render('backend/posts/edit', compact('post'));
+                $this->render('backend/posts/edit', compact('post', 'path_image'));
             }
         }
     }
