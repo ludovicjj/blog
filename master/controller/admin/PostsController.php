aa626c59c5e10ccb90e8395c09e12a472bae216a
Votre nom ou pseudo <jahanlud@gmail.com>
2018-09-04 17:28:46 +0200

    trying again to fix codacy error

--- master/controller/admin/PostsController.php
+++ master/controller/admin/PostsController.php
@@ -31,8 +31,7 @@ class PostsController extends AdminController
                 $_POST['intro'] &&
                 $_POST['content'] &&
                 $_POST['author'] &&
-                $_FILES['image']['name']))
-            {   
+                $_FILES['image']['name'])) {
                 $image = $_FILES['image']['name'];
                 $extensions_valides = array('jpg', 'jpeg', 'png');
                 $extension_upload = strtolower(substr(strrchr($image, '.'), 1));
