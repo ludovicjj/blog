6a64a0fc7621b5bc899fe74e278326b31968db06
Votre nom ou pseudo <jahanlud@gmail.com>
2018-09-04 16:17:29 +0200

    update field image with type file

--- master/view/backend/posts/edit.php
+++ master/view/backend/posts/edit.php
@@ -2,7 +2,7 @@
     <div class="container admin">        
         <a href="index.php?p=admin.posts.index" class="back-admin-index">Retour à la gestion des articles</a>
         
-        <form method="post" class="form-group">
+        <form method="post" class="form-group" enctype="multipart/form-data">
             <legend>Modifier un article</legend>
             <div class="form-group">
                 <label for="title">Titre</label>
@@ -25,9 +25,9 @@
                 class="form-control champs-admin" value="<?= $_SESSION['username'];?>"/>
             </div>
             <div class="form-group">
-                <label for="image">Image</label>
-                <input id="image" name="image" 
-                class="form-control champs-admin" value="<?=$post->getImage();?>"/>
+                <label for="image">Image</label><br/>
+                <img src="<?=$post->getImage();?>" class="admin-eddit-img"/>
+                <input id="image" name="image" type="file"/>
             </div>
             
             <button type="submit"class="btn btn-custom">Envoyer</button>
